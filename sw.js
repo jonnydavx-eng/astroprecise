490a15108434818927be621d931103491c494d3a
jonnydavx-eng <jonnydavx-eng@users.noreply.github.com>
2026-06-14 19:49:55 +0000

    deploy: 4b9bd2d4f4653a052a496225eeb09e5fa195b42f

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Runtime requests use stale-while-revalidate for HTML, cache-first for assets.
  */
 
-const V = 'ap-v113';
+const V = 'ap-v114';
 
 const PRECACHE = [
   './',
@@ -81,6 +81,7 @@ const PRECACHE = [
   './js/vendor/three/jsm/shaders/OutputShader.js',
   './js/chart-render.js',
   './js/sign-daily.js',
+  './js/horoscope-subscribe.js',
   './js/home-sign-picker.js',
   './js/oracle.js',
   './js/profile.js',
